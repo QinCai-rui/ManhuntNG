914213f983cf70ce20c8feea4bc91f1b7f96113c
QinCai-rui <raymontqin_rui@outlook.com>
2026-07-07 10:32:42 +1200

    fix: add missing Iterator import in GameManager

--- src/main/java/xyz/qincai/manhunt/game/GameManager.java
+++ src/main/java/xyz/qincai/manhunt/game/GameManager.java
@@ -10,6 +10,7 @@ import org.bukkit.entity.Player;
 import xyz.qincai.manhunt.ManhuntNG;
 import xyz.qincai.manhunt.player.PlayerRole;
 
+import java.util.Iterator;
 import java.util.UUID;
 import java.util.concurrent.atomic.AtomicInteger;
 
